232076e8bec4a0c5c6795272b09147db312f6696
amrit1951 <amritshaw198@gmail.com>
2022-02-25 17:39:56 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.deepOrange,
+      child:Center(
+        child: Text("Welcome to flutter",
+        textDirection: TextDirection.ltr,
+        )
+        )
+    );
+  }
+}
